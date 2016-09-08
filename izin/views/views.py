da4521821a8dddb595e1c2112c83e84423d3ae4a
unknown <febryyachmad@gmail.com>
2016-09-08 09:48:50 +0700

    pemecahan views izin

---
+++ izin/views/views.py
@@ -0,0 +1,217 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.admin import site
+from functools import wraps
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import available_attrs
+from master.models import Negara, Provinsi, Kabupaten, Kecamatan, Desa, JenisPemohon
+from izin.models import JenisIzin, Syarat, KelompokJenisIzin, JenisPermohonanIzin
+from django.shortcuts import get_object_or_404
+
+import json
+from django.utils.decorators import method_decorator
+
+def passes_test_cache(test_func, timeout=None, using=None, key_prefix=None):
+    def decorator(view_func):
+        @wraps(view_func, assigned=available_attrs(view_func))
+        def _wrapped_view(request, *args, **kwargs):
+            if test_func(request):
+                return cache_page(timeout, cache=using, key_prefix=key_prefix)(view_func)(request, *args, **kwargs)
+            else:
+                return view_func(request, *args, **kwargs)
+        return _wrapped_view
+    return decorator
+
+@passes_test_cache(lambda request: request.user.is_anonymous(), 3600)
+def frontindex(request):
+	return render(request, "front-end/index.html")
+
+def page_404(request):
+	return render(request, "error/404.html")
+
+def frontlogin(request):
+	return render(request, "front-end/login.html")
+
+def tentang(request):
+	return render(request, "front-end/tentang.html")
+
+def layanan(request):
+	return render(request, "front-end/layanan.html")
+
+def cari_pengajuan(request):
+	return render(request, "front-end/cari_pengajuan.html")
+
+def formulir_siup(request, extra_context={}):
+	negara = Negara.objects.all()
+	extra_context.update({'negara': negara})
+	provinsi = Provinsi.objects.all()
+	extra_context.update({'provinsi': provinsi})
+	kabupaten = Kabupaten.objects.all()
+	extra_context.update({'kabupaten': kabupaten})
+	kecamatan = Kecamatan.objects.all()
+	extra_context.update({'kecamatan': kecamatan})
+	desa = Desa.objects.all()
+	extra_context.update({'desa': desa})
+	jenis_pemohon = JenisPemohon.objects.all()
+	extra_context.update({'jenis_pemohon': jenis_pemohon})
+	jenispermohonanizin_list = JenisPermohonanIzin.objects.filter(jenis_izin__id=request.COOKIES['id_kelompok_izin']) # Untuk SIUP
+	extra_context.update({'jenispermohonanizin_list': jenispermohonanizin_list})
+	return render(request, "front-end/formulir/siup.html", extra_context)
+
+def formulir_tdp_pt(request, extra_context={}):
+	negara = Negara.objects.all()
+	extra_context.update({'negara': negara})
+	provinsi = Provinsi.objects.all()
+	extra_context.update({'provinsi': provinsi})
+	kabupaten = Kabupaten.objects.all()
+	extra_context.update({'kabupaten': kabupaten})
+	kecamatan = Kecamatan.objects.all()
+	extra_context.update({'kecamatan': kecamatan})
+	desa = Desa.objects.all()
+	extra_context.update({'desa': desa})
+	jenis_pemohon = JenisPemohon.objects.all()
+	extra_context.update({'jenis_pemohon': jenis_pemohon})
+	return render(request, "front-end/formulir/tdp_pt.html", extra_context)
+
+def formulir_imb_umum(request, extra_context={}):
+	negara = Negara.objects.all()
+	extra_context.update({'negara': negara})
+	provinsi = Provinsi.objects.all()
+	extra_context.update({'provinsi': provinsi})
+	kabupaten = Kabupaten.objects.all()
+	extra_context.update({'kabupaten': kabupaten})
+	kecamatan = Kecamatan.objects.all()
+	extra_context.update({'kecamatan': kecamatan})
+	desa = Desa.objects.all()
+	extra_context.update({'desa': desa})
+	jenis_pemohon = JenisPemohon.objects.all()
+	extra_context.update({'jenis_pemohon': jenis_pemohon})
+	return render(request, "front-end/formulir/imb_umum.html", extra_context)
+
+def formulir_imb_perumahan(request, extra_context={}):
+	negara = Negara.objects.all()
+	extra_context.update({'negara': negara})
+	provinsi = Provinsi.objects.all()
+	extra_context.update({'provinsi': provinsi})
+	kabupaten = Kabupaten.objects.all()
+	extra_context.update({'kabupaten': kabupaten})
+	kecamatan = Kecamatan.objects.all()
+	extra_context.update({'kecamatan': kecamatan})
+	desa = Desa.objects.all()
+	extra_context.update({'desa': desa})
+	jenis_pemohon = JenisPemohon.objects.all()
+	extra_context.update({'jenis_pemohon': jenis_pemohon})
+	return render(request, "front-end/formulir/imb_perumahan.html", extra_context)
+
+def formulir_imb_reklame(request, extra_context={}):
+	negara = Negara.objects.all()
+	extra_context.update({'negara': negara})
+	provinsi = Provinsi.objects.all()
+	extra_context.update({'provinsi': provinsi})
+	kabupaten = Kabupaten.objects.all()
+	extra_context.update({'kabupaten': kabupaten})
+	kecamatan = Kecamatan.objects.all()
+	extra_context.update({'kecamatan': kecamatan})
+	desa = Desa.objects.all()
+	extra_context.update({'desa': desa})
+	jenis_pemohon = JenisPemohon.objects.all()
+	extra_context.update({'jenis_pemohon': jenis_pemohon})
+	return render(request, "front-end/formulir/imb_reklame.html", extra_context)
+
+def formulir_tdp_cv(request, extra_context={}):
+	negara = Negara.objects.all()
+	extra_context.update({'negara': negara})
+	provinsi = Provinsi.objects.all()
+	extra_context.update({'provinsi': provinsi})
+	kabupaten = Kabupaten.objects.all()
+	extra_context.update({'kabupaten': kabupaten})
+	kecamatan = Kecamatan.objects.all()
+	extra_context.update({'kecamatan': kecamatan})
+	desa = Desa.objects.all()
+	extra_context.update({'desa': desa})
+	jenis_pemohon = JenisPemohon.objects.all()
+	extra_context.update({'jenis_pemohon': jenis_pemohon})
+	return render(request, "front-end/formulir/tdp_cv.html", extra_context)
+
+def formulir_tdp_firma(request, extra_context={}):
+	negara = Negara.objects.all()
+	extra_context.update({'negara': negara})
+	provinsi = Provinsi.objects.all()
+	extra_context.update({'provinsi': provinsi})
+	kabupaten = Kabupaten.objects.all()
+	extra_context.update({'kabupaten': kabupaten})
+	kecamatan = Kecamatan.objects.all()
+	extra_context.update({'kecamatan': kecamatan})
+	desa = Desa.objects.all()
+	extra_context.update({'desa': desa})
+	jenis_pemohon = JenisPemohon.objects.all()
+	extra_context.update({'jenis_pemohon': jenis_pemohon})
+	return render(request, "front-end/formulir/tdp_firma.html", extra_context)
+
+def formulir_tdp_perorangan(request, extra_context={}):
+	negara = Negara.objects.all()
+	extra_context.update({'negara': negara})
+	provinsi = Provinsi.objects.all()
+	extra_context.update({'provinsi': provinsi})
+	kabupaten = Kabupaten.objects.all()
+	extra_context.update({'kabupaten': kabupaten})
+	kecamatan = Kecamatan.objects.all()
+	extra_context.update({'kecamatan': kecamatan})
+	desa = Desa.objects.all()
+	extra_context.update({'desa': desa})
+	jenis_pemohon = JenisPemohon.objects.all()
+	extra_context.update({'jenis_pemohon': jenis_pemohon})
+	return render(request, "front-end/formulir/tdp_po.html", extra_context)
+
+def formulir_tdp_koperasi(request, extra_context={}):
+	negara = Negara.objects.all()
+	extra_context.update({'negara': negara})
+	provinsi = Provinsi.objects.all()
+	extra_context.update({'provinsi': provinsi})
+	kabupaten = Kabupaten.objects.all()
+	extra_context.update({'kabupaten': kabupaten})
+	kecamatan = Kecamatan.objects.all()
+	extra_context.update({'kecamatan': kecamatan})
+	desa = Desa.objects.all()
+	extra_context.update({'desa': desa})
+	jenis_pemohon = JenisPemohon.objects.all()
+	extra_context.update({'jenis_pemohon': jenis_pemohon})
+	return render(request, "front-end/formulir/tdp_koperasi.html", extra_context)
+
+def formulir_tdp_bul(request, extra_context={}):
+	negara = Negara.objects.all()
+	extra_context.update({'negara': negara})
+	provinsi = Provinsi.objects.all()
+	extra_context.update({'provinsi': provinsi})
+	kabupaten = Kabupaten.objects.all()
+	extra_context.update({'kabupaten': kabupaten})
+	kecamatan = Kecamatan.objects.all()
+	extra_context.update({'kecamatan': kecamatan})
+	desa = Desa.objects.all()
+	extra_context.update({'desa': desa})
+	jenis_pemohon = JenisPemohon.objects.all()
+	extra_context.update({'jenis_pemohon': jenis_pemohon})
+	return render(request, "front-end/formulir/tdp_bul.html", extra_context)
+
+def identitas_pemohon(request, extra_context={}):
+	nama_lengkap = request.POST.get("nama_lengkap", None)
+	tempat_lahir = request.POST.get("tempat_lahir", None)
+	tanggal_lahir = request.POST.get("tanggal_lahir", None)
+	email = request.POST.get("email", None)
+	provinsi = request.POST.get("provinsi", None)
+	kabupaten = request.POST.get("kabupaten", None)
+	kecamatan = request.POST.get("kecamatan", None)
+	desa = request.POST.get("desa", None)
+	kewarganegaraan = request.POST.get("kewarganegaraan", None)
+	pekerjaan = request.POSt.get("pekerjaan", None)
+	data = {'success': True, 'pesan': 'Simpan data siswa berhasil.', 'id_siswa': siswa.id }
+	return HttpResponse(json.dumps(data))
+
+def cetak_permohonan(request, extra_context={}):
+	return render(request, "front-end/cetak.html", extra_context)
+
+def cetak_bukti_pendaftaran(request, extra_context={}):
+	syarat = Syarat.objects.filter(jenis_izin__jenis_izin__kode="SIUP")
+	extra_context.update({'syarat': syarat})
+	return render(request, "front-end/cetak_bukti_pendaftaran.html", extra_context)
