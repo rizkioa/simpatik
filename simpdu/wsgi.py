99fb4f28c075b0e6cf19acf30d508f28a9857c52
Firman Rosdiansyah <firman.droid@gmail.com>
2016-09-28 11:24:12 +0700

    implement Raven to WSGI

--- simpdu/wsgi.py
+++ simpdu/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simpdu.settings")
 
-application = get_wsgi_application()
+from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+
+application = Sentry(get_wsgi_application())
