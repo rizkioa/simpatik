8c0bcaf2bd6d152eb148ff2b30c7f560cc1434ad
root <root@ubuntu.aitc.co.id>
2016-12-29 15:28:50 +0700

    reklame

--- izin/views/huller.py
+++ izin/views/huller.py
@@ -22,10 +22,10 @@ import os
 
 from master.models import Negara, Kecamatan, JenisPemohon,JenisReklame,Berkas,ParameterBangunan
 from izin.models import JenisIzin, Syarat, KelompokJenisIzin, JenisPermohonanIzin,Riwayat
-from izin.models import PengajuanIzin, DetilHuller,Pemohon
+from izin.models import PengajuanIzin, DetilHuller,Pemohon,MesinHuller,MesinPerusahaan
 from izin.utils import STATUS_HAK_TANAH
 from accounts.models import IdentitasPribadi, NomorIdentitasPengguna
-from izin.izin_forms import UploadBerkasPendukungForm,DetilHullerForm
+from izin.izin_forms import UploadBerkasPendukungForm,DetilHullerForm,MesinHullerForm,MesinPerusahaanForm
 
 def detil_huller_save_cookie(request):
 	if 'id_pengajuan' in request.COOKIES.keys():
@@ -42,6 +42,29 @@ def detil_huller_save_cookie(request):
 				response = HttpResponse(json.dumps(data))
 			else:
 				data = detil_huller.errors.as_json()
+		else:
+			data = {'Terjadi Kesalahan': [{'message': 'Data Pengajuan tidak ditemukan/data kosong'}]}
+			data = json.dumps(data)
+	else:
+		data = {'Terjadi Kesalahan': [{'message': 'Data Pengajuan tidak ditemukan/tidak ada'}]}
+		data = json.dumps(data)
+	response = HttpResponse(data)
+	return response
+
+def mesin_perusahaan_save_cookie(request):
+	if 'id_pengajuan' in request.COOKIES.keys():
+		if request.COOKIES['id_pengajuan'] != '':
+			mesin_perusahaan = MesinPerusahaanForm(request.POST)
+			if mesin_perusahaan.is_valid():
+				mesin_perusahaan.detil_huller  = request.COOKIES['id_pengajuan']
+				mesin_perusahaan.save()
+				data = {'success': True,
+						'pesan': 'Data Reklame berhasil disimpan. Proses Selanjutnya.',
+						'data': ['']}
+				data = json.dumps(data)
+				response = HttpResponse(json.dumps(data))
+			else:
+				data = mesin_perusahaan.errors.as_json()
 		else:
 			data = {'Terjadi Kesalahan': [{'message': 'Data Pengajuan tidak ditemukan/data kosong'}]}
 			data = json.dumps(data)
