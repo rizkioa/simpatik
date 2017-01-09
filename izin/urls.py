c1713e0936df433bf91816e11690f33ce7099302
Febri Ahmad Nurhidayat <febryyachmad@gmail.com>
2017-01-09 15:28:30 +0700

    tdp perorangan

--- izin/urls.py
+++ izin/urls.py
@@ -158,8 +158,8 @@ urlpatterns = [
     url(r'^layanan/tdp-firma/formulir/cetak-bukti-pendaftaran$', views.cetak_bukti_pendaftaran_tdp_firma, name='cetak_bukti_pendaftaran_tdp_firma'),
     
     #cetak TDP PO
-    url(r'^layanan/tdp-perorangan/formulir/cetak$', views.cetak_tdp_po, name='cetak_tdp_po'),
-    url(r'^layanan/tdp-perorangan/formulir/cetak-bukti-pendaftaran$', views.cetak_bukti_pendaftaran_tdp_po, name='cetak_bukti_pendaftaran_tdp_po'),
+    url(r'^layanan/tdp-perorangan/formulir/cetak/(?P<id_pengajuan_>[0-9]+)$', views.cetak_tdp_po, name='cetak_tdp_po'),
+    url(r'^layanan/tdp-perorangan/formulir/cetak-bukti-pendaftaran/(?P<id_pengajuan_>[0-9]+)$', views.cetak_bukti_pendaftaran_tdp_po, name='cetak_bukti_pendaftaran_tdp_po'),
     
     #cetak TDP KOPERASI
     url(r'^layanan/tdp-koperasi/formulir/cetak$', views.cetak_tdp_koperasi, name='cetak_tdp_koperasi'),
