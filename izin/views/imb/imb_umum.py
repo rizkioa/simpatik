d76c00120a262f0a70a1600bc7f113424db8d82b
root <root@ubuntu.aitc.co.id>
2017-01-30 21:47:13 +0700

    imb

--- izin/views/imb/imb_umum.py
+++ izin/views/imb/imb_umum.py
@@ -291,6 +291,7 @@ def load_data_imb(request,id_pengajuan):
 			nama_bangunan = pengajuan_.bangunan
 			luas_bangunan = str(pengajuan_.luas_bangunan)
 			jumlah_bangunan = str(pengajuan_.jumlah_bangunan)
+			id_lokasi = pengajuan_.lokasi
 			if pengajuan_.desa:
 			  id_kecamatan = str(pengajuan_.desa.kecamatan.id)
 			  id_desa = str(pengajuan_.desa.id)
@@ -314,7 +315,7 @@ def load_data_imb(request,id_pengajuan):
 			else:
 				tanggal_imb_lama = ""
 			if nama_bangunan != "":
-				data = {'success': True,'data':{'nama_bangunan': nama_bangunan,'luas_bangunan': luas_bangunan,'jumlah_bangunan': jumlah_bangunan,'luas_tanah': luas_tanah,'status_tanah': status_tanah,'no_surat_tanah': no_surat_tanah,'tanggal_surat_tanah': tanggal_surat_tanah,'luas_bangunan_lama': luas_bangunan_lama,'no_imb_lama': no_imb_lama,'tanggal_imb_lama': tanggal_imb_lama,'id_kecamatan':id_kecamatan,'id_desa':id_desa}}
+				data = {'success': True,'data':{'nama_bangunan': nama_bangunan,'luas_bangunan': luas_bangunan,'jumlah_bangunan': jumlah_bangunan,'luas_tanah': luas_tanah,'status_tanah': status_tanah,'no_surat_tanah': no_surat_tanah,'id_lokasi':id_lokasi,'tanggal_surat_tanah': tanggal_surat_tanah,'luas_bangunan_lama': luas_bangunan_lama,'no_imb_lama': no_imb_lama,'tanggal_imb_lama': tanggal_imb_lama,'id_kecamatan':id_kecamatan,'id_desa':id_desa}}
 			else:
 				id_kecamatan = ""
 				data = {'success': True,'data':{'id_kecamatan':id_kecamatan}}
@@ -333,12 +334,18 @@ def load_identifikasi_bangunan_imb(request,id_pengajuan):
 	if 'id_pengajuan' in request.COOKIES.keys():
 		if request.COOKIES['id_pengajuan'] != '':
 			pengajuan_ = DetilIMB.objects.get(pengajuanizin_ptr_id=request.COOKIES['id_pengajuan'])
-			if pengajuan_.jenis_bangunan != None:
-				if pengajuan_.jenis_bangunan.kode == "BK1":
-					kode_kontruksi_bangunan = str(pengajuan_.jenis_bangunan.kode)
-					id_kontruksi = str(pengajuan_.jenis_bangunan.jenis_kontruksi.id)
-					id_jenis_bangunan = str(pengajuan_.jenis_bangunan.id)
-
+			# print pengajuan_.kelompok_jenis_izin.kode
+			if pengajuan_.jenis_bangunan != None or pengajuan_.kelompok_jenis_izin.kode == "503.01.04/":
+				if pengajuan_.kelompok_jenis_izin.kode == "503.01.04/":
+					kode_izin = pengajuan_.kelompok_jenis_izin.kode
+					if pengajuan_.jenis_bangunan:
+						kode_kontruksi_bangunan = str(pengajuan_.jenis_bangunan.kode)
+						id_kontruksi = str(pengajuan_.jenis_bangunan.jenis_kontruksi.id)
+						id_jenis_bangunan = str(pengajuan_.jenis_bangunan.id)
+					else:
+						kode_kontruksi_bangunan = ""
+						id_kontruksi = ""
+						id_jenis_bangunan = ""
 					kegiatan_pembangunan = pengajuan_.parameter_bangunan.get(parameter="Kegiatan Pembangunan Gedung")
 					nilai_kegiatan_pembangunan = str(kegiatan_pembangunan.nilai)
 					
@@ -364,7 +371,43 @@ def load_identifikasi_bangunan_imb(request,id_pengajuan):
 					nilai_lama_penggunaan_bangunan = str(lama_penggunaan_bangunan.nilai)
 					total_biaya = str(pengajuan_.total_biaya) 
 					data = {'success': True,
-							'data': {'kode_kontruksi_bangunan':kode_kontruksi_bangunan,'id_kontruksi':id_kontruksi,'id_jenis_bangunan':id_jenis_bangunan,'id_jenis_bangunan':id_jenis_bangunan,'nama_fungsi_bangunan': fungsi_bangunan.id,'nilai_fungsi_bangunan': nilai_fungsi_bangunan,'kegiatan_pembangunan': kegiatan_pembangunan.id,'nilai_kegiatan_pembangunan': nilai_kegiatan_pembangunan,'kompleksitas_bangunan': kompleksitas_bangunan.id,'nilai_kompleksitas_bangunan': nilai_kompleksitas_bangunan,'permanensi_bangunan': permanensi_bangunan.id,'nilai_permanensi_bangunan': nilai_permanensi_bangunan,'ketinggian_bangunan': ketinggian_bangunan.id,'nilai_ketinggian_bangunan': nilai_ketinggian_bangunan,'letak_bangunan': letak_bangunan.id,'nilai_letak_bangunan': nilai_letak_bangunan,'kepemilikan_bangunan': kepemilikan_bangunan.id,'nilai_kepemilikan_bangunan': nilai_kepemilikan_bangunan,'lama_penggunaan_bangunan': lama_penggunaan_bangunan.id,'nilai_lama_penggunaan_bangunan': nilai_lama_penggunaan_bangunan,'total_biaya': total_biaya}}
+							'data': {'kode_izin':kode_izin,'kode_kontruksi_bangunan':kode_kontruksi_bangunan,'id_kontruksi':id_kontruksi,'id_jenis_bangunan':id_jenis_bangunan,'id_jenis_bangunan':id_jenis_bangunan,'nama_fungsi_bangunan': fungsi_bangunan.id,'nilai_fungsi_bangunan': nilai_fungsi_bangunan,'kegiatan_pembangunan': kegiatan_pembangunan.id,'nilai_kegiatan_pembangunan': nilai_kegiatan_pembangunan,'kompleksitas_bangunan': kompleksitas_bangunan.id,'nilai_kompleksitas_bangunan': nilai_kompleksitas_bangunan,'permanensi_bangunan': permanensi_bangunan.id,'nilai_permanensi_bangunan': nilai_permanensi_bangunan,'ketinggian_bangunan': ketinggian_bangunan.id,'nilai_ketinggian_bangunan': nilai_ketinggian_bangunan,'letak_bangunan': letak_bangunan.id,'nilai_letak_bangunan': nilai_letak_bangunan,'kepemilikan_bangunan': kepemilikan_bangunan.id,'nilai_kepemilikan_bangunan': nilai_kepemilikan_bangunan,'lama_penggunaan_bangunan': lama_penggunaan_bangunan.id,'nilai_lama_penggunaan_bangunan': nilai_lama_penggunaan_bangunan,'total_biaya': total_biaya}}
+				elif  pengajuan_.jenis_bangunan.kode == "BK1":
+					kode_izin = pengajuan_.kelompok_jenis_izin.kode
+					if pengajuan_.jenis_bangunan:
+						kode_kontruksi_bangunan = str(pengajuan_.jenis_bangunan.kode)
+						id_kontruksi = str(pengajuan_.jenis_bangunan.jenis_kontruksi.id)
+						id_jenis_bangunan = str(pengajuan_.jenis_bangunan.id)
+					else:
+						kode_kontruksi_bangunan = ""
+						id_kontruksi = ""
+						id_jenis_bangunan = ""
+					kegiatan_pembangunan = pengajuan_.parameter_bangunan.get(parameter="Kegiatan Pembangunan Gedung")
+					nilai_kegiatan_pembangunan = str(kegiatan_pembangunan.nilai)
+					
+					fungsi_bangunan = pengajuan_.parameter_bangunan.get(parameter="Fungsi Bangunan")
+					nilai_fungsi_bangunan = str(fungsi_bangunan.nilai)
+
+					kompleksitas_bangunan = pengajuan_.parameter_bangunan.get(parameter="Tingkat Kompleksitas")
+					nilai_kompleksitas_bangunan = str(kompleksitas_bangunan.nilai)
+
+					permanensi_bangunan = pengajuan_.parameter_bangunan.get(parameter="Tingkat Permanensi")
+					nilai_permanensi_bangunan = str(permanensi_bangunan.nilai)
+
+					ketinggian_bangunan = pengajuan_.parameter_bangunan.get(parameter="Ketinggian Bangunan")
+					nilai_ketinggian_bangunan = str(ketinggian_bangunan.nilai)
+
+					letak_bangunan = pengajuan_.parameter_bangunan.get(parameter="Lokasi Bangunan")
+					nilai_letak_bangunan = str(letak_bangunan.nilai)
+
+					kepemilikan_bangunan = pengajuan_.parameter_bangunan.get(parameter="Kepemilikan Bangunan")
+					nilai_kepemilikan_bangunan = str(kepemilikan_bangunan.nilai)
+
+					lama_penggunaan_bangunan = pengajuan_.parameter_bangunan.get(parameter="Lama Penggunaan Bangunan")
+					nilai_lama_penggunaan_bangunan = str(lama_penggunaan_bangunan.nilai)
+					total_biaya = str(pengajuan_.total_biaya) 
+					data = {'success': True,
+							'data': {'kode_izin':kode_izin,'kode_kontruksi_bangunan':kode_kontruksi_bangunan,'id_kontruksi':id_kontruksi,'id_jenis_bangunan':id_jenis_bangunan,'id_jenis_bangunan':id_jenis_bangunan,'nama_fungsi_bangunan': fungsi_bangunan.id,'nilai_fungsi_bangunan': nilai_fungsi_bangunan,'kegiatan_pembangunan': kegiatan_pembangunan.id,'nilai_kegiatan_pembangunan': nilai_kegiatan_pembangunan,'kompleksitas_bangunan': kompleksitas_bangunan.id,'nilai_kompleksitas_bangunan': nilai_kompleksitas_bangunan,'permanensi_bangunan': permanensi_bangunan.id,'nilai_permanensi_bangunan': nilai_permanensi_bangunan,'ketinggian_bangunan': ketinggian_bangunan.id,'nilai_ketinggian_bangunan': nilai_ketinggian_bangunan,'letak_bangunan': letak_bangunan.id,'nilai_letak_bangunan': nilai_letak_bangunan,'kepemilikan_bangunan': kepemilikan_bangunan.id,'nilai_kepemilikan_bangunan': nilai_kepemilikan_bangunan,'lama_penggunaan_bangunan': lama_penggunaan_bangunan.id,'nilai_lama_penggunaan_bangunan': nilai_lama_penggunaan_bangunan,'total_biaya': total_biaya}}
 				elif pengajuan_.jenis_bangunan.kode == "BK2" or pengajuan_.jenis_bangunan.kode == "BK17":
 					kode_kontruksi_bangunan = str(pengajuan_.jenis_bangunan.kode)
 					id_kontruksi = str(pengajuan_.jenis_bangunan.jenis_kontruksi.id)
@@ -395,8 +438,44 @@ def load_konfirmasi_identifikasi_bangunan_imb(request,id_pengajuan):
 	if 'id_pengajuan' in request.COOKIES.keys():
 		if request.COOKIES['id_pengajuan'] != '':
 			pengajuan_ = DetilIMB.objects.get(pengajuanizin_ptr_id=request.COOKIES['id_pengajuan'])
-			if pengajuan_.jenis_bangunan != None:
-				if pengajuan_.jenis_bangunan.kode == "BK1":
+			if pengajuan_.jenis_bangunan != None or pengajuan_.kelompok_jenis_izin.kode == "503.01.04/":
+				if pengajuan_.kelompok_jenis_izin.kode == "503.01.04/":
+					kode_izin = pengajuan_.kelompok_jenis_izin.kode
+					if pengajuan_.jenis_bangunan:
+						kode_kontruksi_bangunan = str(pengajuan_.jenis_bangunan.kode)
+						id_kontruksi = str(pengajuan_.jenis_bangunan.jenis_kontruksi.id)
+						id_jenis_bangunan = str(pengajuan_.jenis_bangunan.id)
+					else:
+						kode_kontruksi_bangunan = ""
+						id_kontruksi = ""
+						id_jenis_bangunan = ""
+					kegiatan_pembangunan = pengajuan_.parameter_bangunan.get(parameter="Kegiatan Pembangunan Gedung")
+					nilai_kegiatan_pembangunan = str(kegiatan_pembangunan.nilai)
+					
+					fungsi_bangunan = pengajuan_.parameter_bangunan.get(parameter="Fungsi Bangunan")
+					nilai_fungsi_bangunan = str(fungsi_bangunan.nilai)
+
+					kompleksitas_bangunan = pengajuan_.parameter_bangunan.get(parameter="Tingkat Kompleksitas")
+					nilai_kompleksitas_bangunan = str(kompleksitas_bangunan.nilai)
+
+					permanensi_bangunan = pengajuan_.parameter_bangunan.get(parameter="Tingkat Permanensi")
+					nilai_permanensi_bangunan = str(permanensi_bangunan.nilai)
+
+					ketinggian_bangunan = pengajuan_.parameter_bangunan.get(parameter="Ketinggian Bangunan")
+					nilai_ketinggian_bangunan = str(ketinggian_bangunan.nilai)
+
+					letak_bangunan = pengajuan_.parameter_bangunan.get(parameter="Lokasi Bangunan")
+					nilai_letak_bangunan = str(letak_bangunan.nilai)
+
+					kepemilikan_bangunan = pengajuan_.parameter_bangunan.get(parameter="Kepemilikan Bangunan")
+					nilai_kepemilikan_bangunan = str(kepemilikan_bangunan.nilai)
+
+					lama_penggunaan_bangunan = pengajuan_.parameter_bangunan.get(parameter="Lama Penggunaan Bangunan")
+					nilai_lama_penggunaan_bangunan = str(lama_penggunaan_bangunan.nilai)
+					total_biaya = str(pengajuan_.total_biaya) 
+					data = {'success': True,
+							'data': {'kode_izin':kode_izin,'kode_kontruksi_bangunan':kode_kontruksi_bangunan,'id_kontruksi':id_kontruksi,'id_jenis_bangunan':id_jenis_bangunan,'id_jenis_bangunan':id_jenis_bangunan,'nama_fungsi_bangunan': fungsi_bangunan.detil_parameter,'nilai_fungsi_bangunan': nilai_fungsi_bangunan,'kegiatan_pembangunan': kegiatan_pembangunan.detil_parameter,'nilai_kegiatan_pembangunan': nilai_kegiatan_pembangunan,'kompleksitas_bangunan': kompleksitas_bangunan.detil_parameter,'nilai_kompleksitas_bangunan': nilai_kompleksitas_bangunan,'permanensi_bangunan': permanensi_bangunan.detil_parameter,'nilai_permanensi_bangunan': nilai_permanensi_bangunan,'ketinggian_bangunan': ketinggian_bangunan.detil_parameter,'nilai_ketinggian_bangunan': nilai_ketinggian_bangunan,'letak_bangunan': letak_bangunan.detil_parameter,'nilai_letak_bangunan': nilai_letak_bangunan,'kepemilikan_bangunan': kepemilikan_bangunan.detil_parameter,'nilai_kepemilikan_bangunan': nilai_kepemilikan_bangunan,'lama_penggunaan_bangunan': lama_penggunaan_bangunan.detil_parameter,'nilai_lama_penggunaan_bangunan': nilai_lama_penggunaan_bangunan,'total_biaya': total_biaya}}
+				elif pengajuan_.jenis_bangunan.kode == "BK1":
 					kode_kontruksi_bangunan = str(pengajuan_.jenis_bangunan.kode)
 					id_kontruksi = pengajuan_.jenis_bangunan.jenis_kontruksi.nama_jenis_kontruksi
 					id_jenis_bangunan = pengajuan_.jenis_bangunan.nama_bangunan
