6793d0a6074f2f445206600bd43f949ed8a29c71
Febri Ahmad Nurhidayat <febryyachmad@gmail.com>
2017-04-16 21:50:59 +0700

    import

--- kepegawaian/views.py
+++ kepegawaian/views.py
@@ -36,4 +36,28 @@ def kirim_notifikasi_telegram(chat_id, pesan, proses, dibuat_oleh, keterangan=""
 		obj.keterangan = keterangan
 		obj.save()
 		return False
-	
+
+def import_pegawai(nama_file):
+	base_file = 'static/import/pegawai/'
+	file = file(base_file+nama_file+'.csv', 'r')
+	c1 = csv.reader(f1) # Data 
+	row = 0 
+	for kl in c1:
+		print '######### START ########'
+		row += 1
+		print "Baris => "+str(row)
+		username = row[0]
+		nama_lengkap = row[1]
+		tempat_lahir = row[2]
+		email = row[5]
+		desa_id = row[6]
+		alamat = row[7]
+		
+		p, created = Pegawai.objects.get_or_create(username=username)
+		p.nama_lengkap = nama_lengkap
+		p.email = email
+		p.desa_id = desa_id
+		p.alamat = alamat
+		p.save()
+		nomor, created = NomorIdentitasPengguna.objects.get_or_create(nomor=nip,user_id=p.id,jenis_identitas_id=1)
+		print '####### END #######'
