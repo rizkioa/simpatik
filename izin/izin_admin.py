7e3ff17b669c4b0555602dd53b9e35ea34b11127
Febri Ahmad Nurhidayat <febryyachmad@gmail.com>
2017-07-11 09:13:40 +0700

    as_option jenis izin

--- izin/izin_admin.py
+++ izin/izin_admin.py
@@ -543,7 +543,7 @@ class IzinAdmin(admin.ModelAdmin):
 	def option_namaizin(self, request):	
 		jenis_izin = request.POST.get('param', None)
 		if jenis_izin:
-			jenisizin_list = JenisIzin.objects.filter(jenis_izin=jenis_izin)
+			jenisizin_list = JenisIzin.objects.filter(jenis_izin=jenis_izin, akrif=True)
 		else:
 			jenisizin_list = JenisIzin.objects.none()
 		pilihan = "<option></option>"
