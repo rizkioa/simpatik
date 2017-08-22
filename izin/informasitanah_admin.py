034b774a6233ab2890b515210253f9e924dbaa31
Febri Ahmad Nurhidayat <febryyachmad@gmail.com>
2017-08-22 10:49:39 +0700

    cetak skizin mobile ippt

--- izin/informasitanah_admin.py
+++ izin/informasitanah_admin.py
@@ -437,7 +437,7 @@ class InformasiTanahAdmin(admin.ModelAdmin):
 		return HttpResponse(template.render(ec))
 
 	def cetak_skizin_ippt_rumah_pdf(self, request, id_pengajuan_izin_):
-		respon = self.cetak_sk_izin_ippt_rumah(self, request, id_pengajuan_izin_)
+		respon = self.cetak_sk_izin_ippt_rumah(self, request, id_pengajuan_izin_, None)
 		return respon
 
 	def cetak_skizin_ippt_usaha_pdf(self, request, id_pengajuan_izin_, salinan_=None):
