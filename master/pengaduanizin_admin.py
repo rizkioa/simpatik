cb942dd046f84e037339fa1eacba389e9139a9ad
arifnehru <nehru.arif13@gmail.com>
2017-09-09 10:30:51 +0700

    modal change-list pengaduan

--- master/pengaduanizin_admin.py
+++ master/pengaduanizin_admin.py
@@ -7,6 +7,6 @@ class PengaduanIzinAdmin(admin.ModelAdmin):
 	list_display = ('no_ktp', 'nama_lengkap', 'kelompok_jenis_izin', 'detil')
 
 	def detil(self, obj):
-		return mark_safe('<a class="btn btn-xs btn-info" tittle="Lihat Detil">Lihat Detil</a>')
+		return mark_safe('<a class="btn btn-xs btn-info" onclick="modal_pengaduan()" tittle="Lihat Detil">Lihat Detil</a>')
 	detil.short_description = 'Aksi'
 admin.site.register(PengaduanIzin, PengaduanIzinAdmin)
