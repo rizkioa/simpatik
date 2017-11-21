a3a1aa5147c80fd297d1c8d6ae2840e724ed46ac
myself <Lightz21>
2017-11-21 10:44:52 +0700

    upload berkas apotek

--- izin_dinkes/urls.py
+++ izin_dinkes/urls.py
@@ -4,6 +4,10 @@ from django.conf.urls import patterns, url, include
 import views
 
 urlpatterns = [
-	url(r'^save-izin-apotek/$', views.save_izin_apotek, name='izin_dinkes__save_izin_apotek'),
+	url(r'^apotek/save-izin-apotek/$', views.save_izin_apotek, name='izin_dinkes__save_izin_apotek'),
+	url(r'^apotek/upload-berkas/$', views.upload_berkas, name='izin_dinkes__upload_berkas'),
+	url(r'^apotek/upload-berkas/save/$', views.apotek_upload_dokumen_cookie, name='izin_dinkes__upload_berkas_save'),
+
 	url(r'^save-izin-toko-obat/$', views.save_izin_toko_obat, name='izin_dinkes__save_izin_toko_obat'),
+	
 ]
