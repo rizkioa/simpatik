abbc4568fe23cfeb6e6290f26f80c7042fdcf22c
myself <Lightz21>
2018-01-08 14:43:09 +0700

    cors

--- static/scripts/js/verifikasi.js
+++ static/scripts/js/verifikasi.js
@@ -40,7 +40,7 @@ function verifikasi_kabid(id_detil_siup, aksi){
 					success: function(response) { // on success..
 						respon = $.parseJSON(response)
 						if(respon.success){
-							send_rekomendasi_dinas()
+							// send_rekomendasi_dinas()
 							toastr["success"](respon.pesan)
 							window.location.href= "";
 						}
